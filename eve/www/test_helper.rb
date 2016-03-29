0221228c1db3fbfcb8b8ef1a285e9f8fb69b0705
miles <miles@glowing.com>
2016-03-29 16:09:33 +0800

    fix

--- eve/www/test_helper.rb
+++ eve/www/test_helper.rb
@@ -13,16 +13,18 @@ module TestHelper
   def magenta_background(text); colorize(text, 45); end
   def exchange_foreground_and_background(text); colorize(text, 7); end
   def light_red(text); colorize(text, 91); end
+ 
 
   def log_msg(msg)
     puts magenta(msg)
   end
+  
   def sky_blue(text); colorize(text, 95); end
   
   def log_important(msg)
     puts sky_blue(msg)
   end
-
+  
   def log_error(msg)
     puts light_red(msg)
   end
--- link_refresh.sh
+++ link_refresh.sh
@@ -1,92 +1,25 @@
-rm -r eve/ios/features/1*
-rm -r eve/ios/features/pages/forum_page.rb
-rm -r eve/ios/features/pages/premium_page.rb
-rm -r eve/ios/features/step_definitions/forum_steps.rb
-rm -r eve/ios/features/step_definitions/premium_steps.rb
-rm -r eve/www/ForumApi.rb
-rm -r eve/www/ForumApiAndroid.rb
-
-rm -r nurture/ios/features/1*
-rm -r nurture/ios/features/pages/forum_page.rb
-rm -r nurture/ios/features/pages/premium_page.rb
-rm -r nurture/ios/features/step_definitions/forum_steps.rb
-rm -r nurture/ios/features/step_definitions/premium_steps.rb
-rm -r nurture/www/ForumApi.rb
-rm -r nurture/www/ForumApiAndroid.rb
-
-rm -r glow/ios/features/1*
-rm -r glow/ios/features/pages/forum_page.rb
-rm -r glow/ios/features/pages/premium_page.rb
-rm -r glow/ios/features/step_definitions/forum_steps.rb
-rm -r glow/ios/features/step_definitions/premium_steps.rb
-rm -r glow/www/ForumApi.rb
-rm -r glow/www/ForumApiAndroid.rb
-
-rm -r baby/ios/features/1*
-rm -r baby/ios/features/pages/forum_page.rb
-rm -r baby/ios/features/pages/premium_page.rb
-rm -r baby/ios/features/step_definitions/forum_steps.rb
-rm -r baby/ios/features/step_definitions/premium_steps.rb
-rm -r baby/www/ForumApi.rb
-rm -r baby/www/ForumApiAndroid.rb
-
-rm -r eve/android/features/1*
-rm -r eve/android/features/pages/forum_page.rb
-rm -r eve/android/features/pages/premium_page.rb
-rm -r eve/android/features/step_definitions/forum_steps.rb
-rm -r eve/android/features/step_definitions/premium_steps.rb
-
-rm -r nurture/android/features/1*
-rm -r nurture/android/features/pages/forum_page.rb
-rm -r nurture/android/features/pages/premium_page.rb
-rm -r nurture/android/features/step_definitions/forum_steps.rb
-rm -r nurture/android/features/step_definitions/premium_steps.rb
-
-rm -r glow/android/features/1*
-rm -r glow/android/features/pages/forum_page.rb
-rm -r glow/android/features/pages/premium_page.rb
-rm -r glow/android/features/step_definitions/forum_steps.rb
-rm -r glow/android/features/step_definitions/premium_steps.rb
-
-rm -r baby/android/features/1*
-rm -r baby/android/features/pages/forum_page.rb
-rm -r baby/android/features/pages/premium_page.rb
-rm -r baby/android/features/step_definitions/forum_steps.rb
-rm -r baby/android/features/step_definitions/premium_steps.rb
-
-ln common/android/features/1* eve/android/features/
-ln common/android/features/1* nurture/android/features/
-ln common/android/features/1* glow/android/features/
-ln common/android/features/1* baby/android/features/
-ln common/android/pages/* nurture/android/features/pages
-
-
-ln common/android/pages/* glow/android/features/pages
-ln common/android/pages/* baby/android/features/pages
-ln common/android/pages/* eve/android/features/pages
-ln common/android/steps/* nurture/android/features/step_definitions
-ln common/android/steps/* eve/android/features/step_definitions
-ln common/android/steps/* glow/android/features/step_definitions
-ln common/android/steps/* baby/android/features/step_definitions
-ln common/ios/features/1* glow/ios/features/
-ln common/ios/features/1* nurture/ios/features/
-ln common/ios/features/1* eve/ios/features/
-ln common/ios/features/1* baby/ios/features/
-ln common/ios/pages/* glow/ios/features/pages
-ln common/ios/pages/* nurture/ios/features/pages
-ln common/ios/pages/* eve/ios/features/pages
-ln common/ios/pages/* baby/ios/features/pages
-ln common/ios/steps/* glow/ios/features/step_definitions
-ln common/ios/steps/* nurture/ios/features/step_definitions
-ln common/ios/steps/* eve/ios/features/step_definitions
-ln common/ios/steps/* baby/ios/features/step_definitions
-ln common/www/ForumApi.rb glow/www/
-ln common/www/ForumApi.rb eve/www/
-ln common/www/ForumApi.rb baby/www/
-ln common/www/ForumApi.rb nurture/www/
-
-ln common/www/ForumApiAndroid.rb glow/www/
-ln common/www/ForumApiAndroid.rb eve/www/
-ln common/www/ForumApiAndroid.rb baby/www/
-ln common/www/ForumApiAndroid.rb nurture/www/
-
+for APP in 'eve' 'glow' 'baby' 'nurture'
+do
+  rm -r ${APP}/ios/features/1*
+  rm -r ${APP}/ios/features/pages/forum_page.rb
+  rm -r ${APP}/ios/features/pages/premium_page.rb
+  rm -r ${APP}/ios/features/step_definitions/forum_steps.rb
+  rm -r ${APP}/ios/features/step_definitions/premium_steps.rb
+  rm -r ${APP}/www/ForumApi.rb
+  rm -r ${APP}/www/ForumApiAndroid.rb
+  rm -r ${APP}/android/features/1*
+  rm -r ${APP}/android/features/pages/forum_page.rb
+  rm -r ${APP}/android/features/pages/premium_page.rb
+  rm -r ${APP}/android/features/step_definitions/forum_steps.rb
+  rm -r ${APP}/android/features/step_definitions/premium_steps.rb
+  rm -r ${APP}/www/test_helper.rb
+  ln common/android/features/1* ${APP}/android/features/
+  ln common/android/pages/* ${APP}/android/features/pages
+  ln common/android/steps/* ${APP}/android/features/step_definitions
+  ln common/ios/features/1* ${APP}/ios/features/
+  ln common/ios/pages/* ${APP}/ios/features/pages
+  ln common/ios/steps/* ${APP}/ios/features/step_definitions
+  ln common/www/ForumApi.rb ${APP}/www/
+  ln common/www/ForumApiAndroid.rb ${APP}/www/
+  ln common/www/test_helper.rb ${APP}/www/
+done
