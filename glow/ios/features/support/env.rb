bd66b736b3b7af91d8da50ccb15d45c476feb442
miles <miles@glowing.com>
2016-05-06 14:15:04 +0800

    add env.file ;

---
+++ glow/ios/features/support/env.rb
@@ -0,0 +1,38 @@
+require 'calabash-cucumber/cucumber'
+require 'minitest/autorun'
+require 'active_support/all'
+require_relative '../../../www/public/glow_ios_forum_test'
+require_relative 'app'
+require_relative 'pages'
+
+include Glow
+include GlowForumIOS
+include Minitest::Assertions
+
+module Pre_config_users
+  def premium_email() "miles3@g.com" end
+  def non_premium_email() "milesn@g.com" end
+  def premium_name() "miles3" end
+  def non_premium_name() "milesn" end
+  module_function :premium_email, :non_premium_email, :premium_name, :non_premium_name
+end
+
+class Myownworld
+  extend Minitest::Assertions
+  include Pre_config_users
+  attr_accessor :assertions
+
+  def initialize
+    self.assertions = 0
+  end
+end
+
+
+World do
+  Myownworld.new
+end
+
+ENV['SCREENSHOT_PATH'] = "./features/screenshots/"
+ENV["APP_BUNDLE_PATH"] = "/Users/Miles/repos/emma/ios/emma_v5.5.app"
+# ENV["DEVICE_TARGET"] = ["97A373AC-E811-473A-A14D-127B4B2EA78A", "BB34C63B-2740-40E7-9889-C8D40D7CB1BB"].sample
+ENV["DEVICE_TARGET"] = "BB34C63B-2740-40E7-9889-C8D40D7CB1BB"
