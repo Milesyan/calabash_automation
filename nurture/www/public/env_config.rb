70ed73c9edf7513f238287f4b6d496fa9912da8b
miles <miles@glowing.com>
2016-05-16 14:48:46 +0800

    fix for android

--- nurture/www/public/env_config.rb
+++ nurture/www/public/env_config.rb
@@ -17,11 +17,13 @@ module AndroidConfig
   extend TestHelper
   if APP_CONFIG.end_with? 'local'
     def base_url() load_config["base_urls"]["#{APP_CONFIG}"] end
+    def forum_base_url() load_config["base_urls"]["Forum-local"] end
+    def bryo_url() load_config["base_urls"]["Bryo-local"] end
   else 
     def base_url() load_config["base_urls"]["#{APP_CONFIG}-www1"] end
+    def forum_base_url() load_config["base_urls"]["Forum-www1"] end
+    def bryo_url() load_config["base_urls"]["Bryo-www1"] end
   end
-  def forum_base_url() load_config["base_urls"]["Forum-www1"] end
-  def bryo_url() load_config["base_urls"]["Bryo-www1"] end
 
   module_function :base_url, :forum_base_url, :bryo_url
 end
